0a6bc239b0c2a3dbf044b6baefe6a6832c91a402
gakubochan <shinoda.g.aa@m.titech.ac.jp>
2022-04-20 01:55:42 +0900

    14_bucket_sort.cppの修正

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -20,16 +20,13 @@ int main() {
   for (int i=1; i<range; i++) 
     offset[i] = offset[i-1] + bucket[i-1];
 
-#pragma omp parallel
-{
-  #pragma omp for 
+  #pragma omp parallel for  
   for (int i=0; i<range; i++) {
-    int j = offset[i];
+    int j = offset[i];    
     for (; bucket[i]>0; bucket[i]--) {
       key[j++] = i;
     }
   }
-}
   for (int i=0; i<n; i++) {
     printf("%d ",key[i]);
   }
